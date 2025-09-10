a95132e72a62a019d6f0fcf9985b615dd80b4f87
Yun Fok <adrianfok26@gmail.com>
2025-09-10 21:47:50 +0800

    [Create-Game-Page] Created Types LetterResult, GuessResult and GameState

---
+++ frontend/src/types/core.ts
@@ -0,0 +1,19 @@
+export interface LetterResult {
+  letter: string;
+  status: 'correct' | 'wrong-position' | 'incorrect';
+}
+
+export interface GuessResult {
+  guessWord: string;
+  letterResults: LetterResult[];
+  isCorrect: boolean;
+}
+
+export interface GameState {
+  currentGuessWord: string;
+  tries: GuessResult[];
+  gameStatus: 'playing' | 'won' | 'lost';
+  targetWord: string;
+  maxTries: number;
+  mode: 'classic' | 'custom';
+}
